12f75a4f903a2a7d4d70d8d9202b31551b3b4a1d
Artem Bagno <bgn.rtm@gmail.com>
2026-06-09 10:20:11 +0300

    fixed bag js dataset and margin-scroll

--- src/js/about.js
+++ src/js/about.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const aboutSection = document.querySelector('.about');
+const pictureWrapper = document.querySelector('[data-observer]');
 
-  const observer = new IntersectionObserver(
-    (entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('visible');
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    {
-      threshold: 0.1,
-    }
-  );
-
-  if (aboutSection) {
-    observer.observe(aboutSection);
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.dataset.observer = 'true';
+      } else {
+        entry.target.dataset.observer = 'false';
+      }
+    });
+  },
+  {
+    threshold: 0.1,
   }
-});
+);
+if (pictureWrapper) {
+  observer.observe(pictureWrapper);
+}
--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -4,14 +4,14 @@ const modalViewEl = document.querySelector('[data-visible]');
 const navListEl = document.querySelector('[data-menu-navigation]');
 
 openBtnEl.addEventListener('click', () => {
-  modalViewEl.setAttribute('data-visible', 'open');
+  modalViewEl.dataset.visible = 'open';
   closeBtnEl.addEventListener('click', handleClose);
   document.addEventListener('keydown', handleEscClose);
   document.body.classList.add('no-scroll');
 });
 
 const handleClose = () => {
-  modalViewEl.setAttribute('data-visible', 'close');
+  modalViewEl.dataset.visible = 'close';
   document.body.classList.remove('no-scroll');
   closeBtnEl.removeEventListener('click', handleClose);
   document.removeEventListener('keydown', handleEscClose);
